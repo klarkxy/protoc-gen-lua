8f3dffc6c3f90ad205420680b192650baa6b1e7b
klarkxy <278370456@qq.com>
2021-12-02 17:05:05 +0800

    从旧版protoc生成一版protoc自带的google/protobuf

--- plugin/gen-lua-proto.py
+++ plugin/gen-lua-proto.py
@@ -4,11 +4,12 @@ import sys
 PWD = os.path.abspath(os.path.split(__file__)[0])
 PROTOC_DIR_PATH = os.path.join(PWD, "protoc")
 INCLUDE_PATH = os.path.join(PROTOC_DIR_PATH, "include")
+LUA_OUT_PATH = os.path.join(PWD, "..")
 PROTOC = os.path.join(PROTOC_DIR_PATH, "bin", "protoc")
 if sys.platform == "win32":
     PROTOC_GEN_LUA = os.path.join(PWD, "protoc-gen-lua.bat")
 else:
-    PROTOC_GEN_LUA = os.path.join(PWD, "protoc-gen-lua.py")
+    PROTOC_GEN_LUA = os.path.join(PWD, "protoc-gen-lua")
 
 real_files = []
 for root, dirs, files in os.walk(INCLUDE_PATH):
@@ -17,6 +18,6 @@ for root, dirs, files in os.walk(INCLUDE_PATH):
             realfile = os.path.join(root, file)
             real_files.append(realfile)
 
-cmd = f"{PROTOC} --plugin=protoc-gen-lua={PROTOC_GEN_LUA} --lua_out={INCLUDE_PATH} --proto_path={INCLUDE_PATH} " + " ".join(real_files)
+cmd = f"{PROTOC} --plugin=protoc-gen-lua={PROTOC_GEN_LUA} --lua_out={LUA_OUT_PATH} --proto_path={INCLUDE_PATH} " + " ".join(real_files)
 print(cmd)
 os.system(cmd)
